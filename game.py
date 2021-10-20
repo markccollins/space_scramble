04c2a8312e107372395b86e0378f5e981fea5d53
capnsquishy <kramsnilloc@gmail.com>
2021-10-19 22:19:57 -0500

    restructured how rendering is performed, and experimenting with how views are interpreted

--- game.py
+++ game.py
@@ -5,125 +5,49 @@ import os
 import pygame
 from pygame import Rect
 from pygame.math import Vector2
+from views import title_screen
 
 
-# stores data on current game state
-class GameState():
-    def __init__(self):
-        self.world_size = Vector2(16, 10)
-        self.views = ['title', 'menu', 'resource', 'ship']      # decides which view we are rendering and updating for
-        self.view = 'title'
-
-        # title variables
-        self.title_option_position = 0
-
-    def update(self, event):
-        if self.view is 'title':
-            if event.key == pygame.K_DOWN:
-                if self.title_option_position < 2:
-                    self.title_option_position += 1
-            elif event.key == pygame.K_UP:
-                if self.title_option_position > 0:
-                    self.title_option_position += -1
-
-        if self.view is 'menu':
-            pass
-
-        if self.view is 'resource':
-            pass
-
-        if self.view is 'ship':
-            pass
-
-
-class Interface():
+class Interface:
     def __init__(self):
         pygame.init()
 
-        self.game_state = GameState()
-
         self.cell_size = Vector2(64, 64)
-        # self.units_texture = pygame.image.load('units.png')
+        self.world_size = Vector2(16, 10)
+
+        self.views = {          # decides which view we are rendering and updating for
+            'title': None,
+            'menu': None,
+            'resource': None,
+            'ship': None
+        }
+        self.view = 'title'
 
         # initialize window settings
-        window_size = self.game_state.world_size.elementwise() * self.cell_size
+        window_size = self.world_size.elementwise() * self.cell_size
         self.window = pygame.display.set_mode((int(window_size.x), int(window_size.y)))
         pygame.display.set_caption('space scramble')
-
-        # load default textures
-        self.title_bg = pygame.image.load(os.path.join('sprites', 'title_screen.png'))
-        self.title_rocket = pygame.image.load(os.path.join('sprites', 'rocket.png'))
-        self.star_tick_1 = 0
-        self.star_tick_2 = 0
-        self.star_tick_3 = 0
-        self.title_stars_1 = pygame.image.load(os.path.join('sprites', 'stars_1.png'))
-        self.title_stars_2 = pygame.image.load(os.path.join('sprites', 'stars_2.png'))
-        self.title_stars_3 = pygame.image.load(os.path.join('sprites', 'stars_3.png'))
 
         # game loop settings
         self.clock = pygame.time.Clock()
         self.running = True
+
+        # initialize title view
+        self.views['title'] = title_screen.TitleScreenView(self.window, self.cell_size)
 
     def process_input(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
                 break
-            elif event.type == pygame.KEYDOWN:
-                self.game_state.update(event)
-                pass
 
     def update(self):
-        if self.game_state.view is 'title':
-            self.star_tick_1 += 1
-            self.star_tick_2 += 2
-            self.star_tick_3 += 3
-            if self.star_tick_1 > self.title_stars_1.get_height():
-                self.star_tick_1 = 0
-            if self.star_tick_2 > self.title_stars_2.get_height():
-                self.star_tick_2 = 0
-            if self.star_tick_3 > self.title_stars_3.get_height():
-                self.star_tick_3 = 0
-
-    def render(self):
-        self.update()
-        self.window.fill((0, 0, 0))
-
-        if self.game_state.view is 'title':
-            # draw title background
-            self.window.blit(self.title_bg, (0, 0))
-
-            # draw star background
-            self.window.blit(self.title_stars_1, (0, self.star_tick_1))
-            self.window.blit(self.title_stars_1, (0, self.title_stars_1.get_height() * -1 + self.star_tick_1))
-
-            self.window.blit(self.title_stars_2, (0, int(self.star_tick_2)))
-            self.window.blit(self.title_stars_2, (0, int(self.title_stars_2.get_height() * -1 + self.star_tick_2)))
-
-            self.window.blit(self.title_stars_2, (0, int(self.star_tick_3 )))
-            self.window.blit(self.title_stars_2, (0, int(self.title_stars_3.get_height() * -1 + self.star_tick_3)))
-
-            # draw rocket cursor
-            sprite_rect = Rect(0, 0, int(self.cell_size.x), int(self.cell_size.y))
-            self.window.blit(self.title_rocket, (int(5.5 * self.cell_size.x),
-                int((4.15 * self.cell_size.y) + self.game_state.title_option_position * 80)), sprite_rect)
-
-        if self.game_state.view is 'menu':
-            pass
-
-        if self.game_state.view is 'resource':
-            pass
-
-        if self.game_state.view is 'ship':
-            pass
-
-        pygame.display.update()
+        self.views[self.view].update()
 
     def run(self):
         while self.running:
             self.process_input()
             self.update()
-            self.render()
             self.clock.tick(60)
 
 
---
+++ views/title_screen.py
@@ -0,0 +1,76 @@
+import os
+import pygame
+from pygame import Rect
+
+
+class TitleScreenView:
+    def __init__(self, window, cell_size):
+        # initialize window variables
+        self.window = window
+        self.cell_size = cell_size
+
+        # load background textures
+        self.title_bg = pygame.image.load(os.path.join('sprites', 'title_screen.png'))
+        self.title_stars_1 = pygame.image.load(os.path.join('sprites', 'stars_1.png'))
+        self.title_stars_2 = pygame.image.load(os.path.join('sprites', 'stars_2.png'))
+        self.title_stars_3 = pygame.image.load(os.path.join('sprites', 'stars_3.png'))
+
+        # load rocket cursor
+        self.title_rocket = pygame.image.load(os.path.join('sprites', 'rocket.png'))
+
+        # initialize star background timers
+        self.star_tick_1 = 0
+        self.star_tick_2 = 0
+        self.star_tick_3 = 0
+
+        # title variables
+        self.title_option_position = 0
+
+    def process_input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN:
+                    if self.title_option_position < 2:
+                        self.title_option_position += 1
+                elif event.key == pygame.K_UP:
+                    if self.title_option_position > 0:
+                        self.title_option_position += -1
+
+    def update(self):
+        self.process_input()
+
+        self.star_tick_1 += 1
+        self.star_tick_2 += 2
+        self.star_tick_3 += 3
+        if self.star_tick_1 > self.title_stars_1.get_height():
+            self.star_tick_1 = 0
+        if self.star_tick_2 > self.title_stars_2.get_height():
+            self.star_tick_2 = 0
+        if self.star_tick_3 > self.title_stars_3.get_height():
+            self.star_tick_3 = 0
+
+        self.render()
+
+    def render(self):
+        # clear screen with blank window to prepare for render
+        self.window.fill((0, 0, 0))
+
+        # draw title background
+        self.window.blit(self.title_bg, (0, 0))
+
+        # draw star background
+        self.window.blit(self.title_stars_1, (0, self.star_tick_1))
+        self.window.blit(self.title_stars_1, (0, self.title_stars_1.get_height() * -1 + self.star_tick_1))
+
+        self.window.blit(self.title_stars_2, (0, int(self.star_tick_2)))
+        self.window.blit(self.title_stars_2, (0, int(self.title_stars_2.get_height() * -1 + self.star_tick_2)))
+
+        self.window.blit(self.title_stars_3, (0, int(self.star_tick_3)))
+        self.window.blit(self.title_stars_3, (0, int(self.title_stars_3.get_height() * -1 + self.star_tick_3)))
+
+        # draw rocket cursor
+        sprite_rect = Rect(0, 0, int(self.cell_size.x), int(self.cell_size.y))
+        self.window.blit(self.title_rocket, (int(5.5 * self.cell_size.x),
+                        int((4.15 * self.cell_size.y) + self.title_option_position * 80)),sprite_rect)
+
+        pygame.display.update()
